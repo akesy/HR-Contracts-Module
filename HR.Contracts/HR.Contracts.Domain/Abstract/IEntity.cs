7575b5493fc655c220073e0ba95d559e5c5e8a46
Adam Kęsy <akesy@onet.pl>
2015-09-23 17:47:13 +0200

    Brakujące pliki.

---
+++ HR.Contracts/HR.Contracts.Domain/Abstract/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace HR.Contracts.Domain.Abstract
+{
+    interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
---
+++ HR.Contracts/HR.Contracts.WebUI/Controllers/ContractController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace HR.Contracts.WebUI.Controllers
+{
+    public class ContractController : Controller
+    {
+        public ViewResult List()
+        {
+            return this.View();
+        }
+    }
+}
